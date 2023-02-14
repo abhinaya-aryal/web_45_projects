65a29833bed4550fbb75985cce9683d28cd5e1e0
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-02-14 20:58:50 +0545

    blurry_loading completed

--- blurry_loading/script.js
+++ blurry_loading/script.js
@@ -1,1 +1,28 @@
-console.log('Hello! Dev');
+const background = document.querySelector('.background');
+const label = document.querySelector('.label');
+
+let load = 0;
+
+
+const scale = (number, inMin, inMax, outMin, outMax) =>{
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+const effect = () =>{
+  load++;
+  console.log(load);
+  if(load > 99){
+    clearInterval(blurring);
+  }
+
+  label.innerText = `${load}%`;
+  label.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const blurring = setInterval(effect, 20);
+
+
+
+
+
