bc5d118bdfbd46e22a343f38442c8da74dfdbf57
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-02-14 12:45:51 +0545

    hidden_search_widget completed

--- hidden_search_widget/script.js
+++ hidden_search_widget/script.js
@@ -1,1 +1,8 @@
-console.log('Hello! Dev');
+const search = document.querySelector('.search');
+const button = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+button.addEventListener('click', ()=>{
+  search.classList.toggle('active');
+  input.focus();
+})
