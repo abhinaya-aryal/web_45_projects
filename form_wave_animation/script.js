29b50a25d4f3a2f6cde460d89068867280dcda01
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-02-15 22:04:54 +0545

    form_wave_animation completed

--- form_wave_animation/script.js
+++ form_wave_animation/script.js
@@ -1,1 +1,9 @@
-console.log('Hello! Dev');
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label =>{
+  let letters = label.innerText.split('');
+  letters = letters.map((letter, index) =>{
+    return `<span style="transition-delay: ${index * 40}ms">${letter}</span>`;
+  })
+  label.innerHTML = letters.join('');
+})
