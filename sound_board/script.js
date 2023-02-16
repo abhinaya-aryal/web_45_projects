1d356366d703c47177dc752853b6f95c7081bf56
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-02-16 19:25:49 +0545

    sound_board completed

--- sound_board/script.js
+++ sound_board/script.js
@@ -1,1 +1,23 @@
-console.log('Hello! Dev');
+const sounds = ['boo', 'gasp', 'tada', 'applause', 'victory', 'wrong'];
+
+const stopSongs = () => {
+  sounds.forEach(sound =>{
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  })
+}
+
+sounds.forEach(sound =>{
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+
+  btn.addEventListener('click', ()=>{
+    stopSongs();
+    document.getElementById(sound).play();
+  })
+  document.querySelector('.box-container').append(btn);
+})
+
+
