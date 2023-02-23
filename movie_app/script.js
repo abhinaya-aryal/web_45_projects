cc7d024098616ab15db142125379a726292c814c
Abhinaya Aryal <aryalabhinaya343@gmail.com>
2023-02-23 13:55:08 +0545

    button_ripple_effect completed

--- movie_app/script.js
+++ movie_app/script.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ddcbbae4845822bd85492e166a0a6d2e&page=1"; 
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=ddcbbae4845822bd85492e166a0a6d2e&query="';
 
 const form = document.getElementById('form');
